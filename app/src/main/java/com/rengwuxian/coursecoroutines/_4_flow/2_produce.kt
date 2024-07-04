b083ef76472b89b5b4ffea691810f2f1a4840419
扔物线-朱凯 <rengwuxian@gmail.com>
2024-07-04 16:16:30 +0800

    4.2-用 produce() 来提供跨协程的事件流

---
+++ app/src/main/java/com/rengwuxian/coursecoroutines/_4_flow/2_produce.kt
@@ -0,0 +1,29 @@
+package com.rengwuxian.coursecoroutines._4_flow
+
+import com.rengwuxian.coursecoroutines.common.gitHub
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.EmptyCoroutineContext
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun main() = runBlocking<Unit> {
+  val scope = CoroutineScope(EmptyCoroutineContext)
+  val receiver = scope.produce {
+    while (isActive) {
+      val data = gitHub.contributors("square", "retrofit")
+      send(data)
+    }
+  }
+  launch {
+    delay(5000)
+    while (isActive) {
+      println("Contributors: ${receiver.receive()}")
+    }
+  }
+  delay(10000)
+}
