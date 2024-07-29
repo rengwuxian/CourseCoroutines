14f94e31ca86c114e98e4ba68dabddb6e88d9166
扔物线-朱凯 <rengwuxian@gmail.com>
2024-07-29 15:06:35 +0800

    4.20-chunked() 操作符

---
+++ app/src/main/java/com/rengwuxian/coursecoroutines/_4_flow/20_chunked.kt
@@ -0,0 +1,21 @@
+package com.rengwuxian.coursecoroutines._4_flow
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.chunked
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.EmptyCoroutineContext
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun main() = runBlocking<Unit> {
+  val scope = CoroutineScope(EmptyCoroutineContext)
+  val flow1 = flowOf(1, 2, 3, 4, 5)
+  scope.launch {
+    flow1.chunked(2).collect { println("chunked: $it") }
+  }
+  delay(10000)
+}
