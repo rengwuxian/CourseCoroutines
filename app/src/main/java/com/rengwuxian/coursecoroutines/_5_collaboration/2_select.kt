0819d783437e0d847df86459a765a8175302a0c8
扔物线-朱凯 <rengwuxian@gmail.com>
2024-08-29 12:06:11 +0800

    5.2-select()：先到先得

---
+++ app/src/main/java/com/rengwuxian/coursecoroutines/_5_collaboration/2_select.kt
@@ -0,0 +1,59 @@
+package com.rengwuxian.coursecoroutines._5_collaboration
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.selects.onTimeout
+import kotlinx.coroutines.selects.select
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.time.Duration.Companion.seconds
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun main() = runBlocking {
+  val scope = CoroutineScope(EmptyCoroutineContext)
+  val job1 = launch {
+    delay(1000)
+    println("job1 done")
+  }
+  val job2 = launch {
+    delay(2000)
+    println("job2 done")
+  }
+  val deferred = async {
+    delay(500)
+    "rengwuxian"
+  }
+  val channel = Channel<String>()
+  scope.launch {
+    val result = select {
+      job1.onJoin {
+        delay(2000)
+        1
+      }
+      job2.onJoin {
+        2
+      }
+      deferred.onAwait {
+        it
+      }
+      channel.onSend("haha") {
+
+      }
+      channel.onReceive {
+
+      }
+      channel.onReceiveCatching {
+
+      }
+      onTimeout(1.seconds) {
+
+      }
+    }
+    println("Result: $result")
+  }
+  delay(10000)
+}
